c89ea81a681ee6e5493ccdcbfa3f6d1835d92677
benimustiko <indologian@gmail.com>
2024-09-21 04:09:40 +0700

    change portfolio grid col

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <!-- cards -->
-    <div class="container mx-auto grid grid-cols-2 gap-x-4 gap-y-5 pt-3 pb-12 px-6">
+    <div class="container mx-auto grid grid-cols-2 md:grid-cols-4 md:max-w-4xl gap-x-4 gap-y-5 pt-3 pb-12 px-6">
       <PortfolioCard />
       <PortfolioCard />
       <PortfolioCard />
