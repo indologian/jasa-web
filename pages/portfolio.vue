bb9405510996c954e500ec7c7181c0fac8116913
benimustiko <indologian@gmail.com>
2024-09-21 13:47:53 +0700

    fix portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <!-- cards -->
-    <div class="container mx-auto grid grid-cols-2 md:grid-cols-4 lg:max-w-7xl gap-x-4 gap-y-5 pt-3 pb-12 px-6">
+    <div class="container mx-auto grid grid-cols-2 md:grid-cols-4 lg:max-w-6xl gap-x-4 gap-y-5 pt-3 pb-12 px-6">
       <PortfolioCard />
       <PortfolioCard />
       <PortfolioCard />
